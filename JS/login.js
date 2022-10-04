31ca3641b60b74edbd90278a14bd062a50ab57c1
PigPenguinMan <bigpenguinman@naver.com>
2022-10-04 15:12:02 +0900

    jQuery  commit

--- JS/login.js
+++ JS/login.js
@@ -1,20 +1,20 @@
 // 입력폼에 작성한 이름 가져오기 - form의 submit을 이벤트를 사용하기위해 
-const loginForm = document.querySelector("#login_Form")
+const loginForm = $("#login_Form");
 // name 값을 가져오기 위한 input 
-const loginInput = document.querySelector("#login_Form #name");
+const loginInput = $("#login_Form #name");
 // name 값을 가질 h1
-const title = document.querySelector("#title");
+const title = $("#title");
 
 // eventListener 연결 
 // formdml submit을 사용할 경우 click이벤트가 아니라 form의 submit 이벤트로 연결해줘야 한다 
 
-loginForm.addEventListener("submit",onLoginSubmit);
+loginForm.on("submit",onLoginSubmit);
 
 function onLoginSubmit(e){
     e.preventDefault(); // 새로고침 취소
     
     // loginInput의 value값을 가져옴
-    const name = loginInput.value
+    const name = loginInput.val();
 
     // if 문을 사용해서 , name값이 없을때 , 아래 실행되지않게
     // 함수에서 return을 사용하면 함수 종료 
@@ -24,9 +24,9 @@ function onLoginSubmit(e){
     }
 
     // value값을 title에 넣어줌
-    title.innerHTML = name+"님 반갑습니다"; 
+    title.html(name+"님 반갑습니다"); 
     // login-Form  hidden 클래스 추가 
-    loginForm.classList.add("hidden");
+    loginForm.addClass("hidden");
     // todo-Form hidden 클래스 제거
-    document.querySelector("#todo_Form").classList.remove("hidden")
+    $("#todo_Form").removeClass("hidden")
 }
--- JS/todolist.js
+++ JS/todolist.js
@@ -1,49 +1,48 @@
 // todo 입력 공간 -
-const todoInput = document.querySelector("#todo_Form #todo");
+const todoInput = $("#todo_Form #todo");
 
 // button : click 이벤트로 실행
 // button에 id가 없기떄문에 , 노드로 접근하거나.
 // querySelector를 이용해 선택자를 이용하여 접근해서 객체를 가져온다 
-const todoButton =todoInput.nextElementSibling;
+const todoButton = todoInput.next();
 
 // todo board 객체 가져오기
-const todoBoard = document.querySelector("#todo_board");
+const todoBoard = $("#todo_board");
 
 // 버튼에 이벤트 리스너 : button 이므로 click이벤트 
-todoButton.addEventListener("click",addTodo);
+todoButton.on("click",addTodo);
 
 function addTodo(){
     // todoInput - value 값 들고오기 
-    const text = todoInput.value;
+    const text = todoInput.val();
 
     // 새로 요소 생성 
     // li , input type= "checkbox" ,textnode , button
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    const textNode = document.createTextNode(text);
-    const button = document.createElement("button")
-    button.innerHTML = "X";
+    const li = $('<li>')
+    const checkbox = $('<input>').attr('type','checkbox');
+    const textbox = $('<span>').html(text);
+    const button = $("<button>");
+    button.html("X");
     // li요소 - input ,textnode , button 추가
-    li.appendChild(checkbox);
-    li.appendChild(textNode);
-    li.appendChild(button);
+    li.append(checkbox);
+    li.append(textbox);
+    li.append(button);
     // todoBoard에 li추가
     todoBoard.append(li);
 
     // todoInput.value 값을 빈값으로 만들기 
-    todoInput.value = "";
+    todoInput.val("");
 
     // 체크 박스를 눌렀을때 이벤트 리스너 실행 :click
-    checkbox.addEventListener("click",todoCheck)
+    checkbox.on("click",todoCheck)
 
    // X버튼을 눌러 삭제
-    button.addEventListener("click",removeBtn)
+    button.on("click",removeBtn)
 }
 
  
  function removeBtn(e){
-    const li = e.target.parentNode;
+    const li = e.target.parent();
     li.remove();
  }
 // 체크박스 이벤트 리스너에 들어가는 함수
